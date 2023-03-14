c65f0bece15fab5aab6816aae36d1647d737f829
mabruk7 <maruk33@gmail.com>
2023-03-13 17:05:49 -0700

    concat

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,9 +1,5 @@
 #ifndef MAIN_H
-		
 #define MAIN_H
-		
-
-		
 char *create_array(unsigned int size, char c);
 		
 int _putchar(char c);
@@ -17,7 +13,4 @@ int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
 		
 char *argstostr(int ac, char **av);
-		
-
-		
 #endif
