9639b6939aaf8d423891e5d24e3a1a7adbaee6cf
mabruk7 <maruk33@gmail.com>
2023-03-13 18:09:36 -0700

    1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,13 +14,10 @@ if (str == NULL)
 return (NULL);		
 i = 0;
 while (str[i] != '\0')
-		
 		i++;		
-		
 aaa = malloc(sizeof(char) * (i + 1));		
-		
 if (aaa == NULL)
-return (NULL);	
+return (NULL);
 for (r = 0; str[r]; r++)
 aaa[r] = str[r];
 return (aaa);
