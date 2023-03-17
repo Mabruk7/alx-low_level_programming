b6a1a86198408d2b012f6da05611eb4e21c2cbd7
mabruk7 <maruk33@gmail.com>
2023-03-17 12:31:44 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -23,19 +23,18 @@ int is_digit(char *s)
 	}
 	return (1);
 }
-
 /**
 * _strlen - returns the length of a string
 * @s: string to evaluate
-*		
-* Return: the length of the string		
+*
+* Return: the length of the string
 */
 
 int _strlen(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
-		
 	{
 		i++;
 	}
@@ -46,7 +45,7 @@ int _strlen(char *s)
 /**
 * errors - handles errors for main
 */
-		
+
 void errors(void)
 {
 	printf("Error\n");
@@ -60,12 +59,13 @@ void errors(void)
 *
 * Return: always 0 (Success)
 */
-		
+
 int main(int argc, char *argv[])
 
 {
 	char *s1, *s2;
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
+
 	s1 = argv[1], s2 = argv[2];
 
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
@@ -79,7 +79,7 @@ int main(int argc, char *argv[])
 		return (1);
 	for (i = 0; i <= len1 + len2; i++)
 		result[i] = 0;
-	for (len1 = len1 - 1; len1 >= 0; len1--)	
+	for (len1 = len1 - 1; len1 >= 0; len1--)
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
@@ -90,11 +90,9 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
-
 		if (carry > 0)
 			result[len1 + len2 + 1] += carry;
 	}
-
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
@@ -102,7 +100,6 @@ int main(int argc, char *argv[])
 		if (a)
 			_putchar(result[i] + '0');
 	}
-
 	if (!a)
 		_putchar('0');
 	_putchar('\n');
